8bc7def822ec8324b10e62b25eba9a9f414fb99f
DmShko <vombat_schev@i.ua>
2023-10-29 22:40:50 +0200

    goit-react-hw-07-phonebook - 08

--- src/components/DataIn/DataIn.js
+++ src/components/DataIn/DataIn.js
@@ -11,8 +11,8 @@ import { getAPI } from 'API/GetContacts';
 export const DataIn = () => {
   const [name, setName] = useState('');
   const [phone, setNumber] = useState('');
-  const [inputNameValid, setInputNameValid] = useState('');
-  const [inputNumberValid, setInputNumberValid] = useState('');
+  // const [inputNameValid, setInputNameValid] = useState('');!!!!!!!!!!!!!!!!
+  // const [inputNumberValid, setInputNumberValid] = useState('');!!!!!!!!!!!!!!!!!!
 
   const dispatch = useDispatch();
   const selector = useSelector(state => state.phonebook.items);
@@ -25,7 +25,7 @@ export const DataIn = () => {
   // transfer contacts data to method in App 'reducer'
   const addUser = evt => {
     // transfer data only, if valid fields is valid
-    if (inputNameValid === false && inputNumberValid === false) {
+    // if (inputNameValid === false && inputNumberValid === false) {!!!!!!!!!!!!!!!!!!!!!!
       evt.preventDefault(getAPI);
 
       if (
@@ -44,25 +44,25 @@ export const DataIn = () => {
       }
 
       // dispatch(add({name, phone,}));
-    } else {
-      evt.preventDefault();
-    }
+    // } else {!!!!!!!!!!!!!
+    //   evt.preventDefault();!!!!!!!!!!!!!!
+    // }!!!!!!!!!!!!!!
 
     clearInputs();
   };
 
-  const checkValid = data => {
-    // CHANGE 'VALID' PROPERTIES OF inputs ON BASE PREVIOUS VALUE
-    if (data.validity.patternMismatch === false) {
-      data.name === 'name'
-        ? setInputNameValid(value => value && data.validity.patternMismatch)
-        : setInputNumberValid(value => value && data.validity.patternMismatch);
-    } else {
-      data.name === 'name'
-        ? setInputNameValid(value => value || data.validity.patternMismatch)
-        : setInputNumberValid(value => value || data.validity.patternMismatch);
-    }
-  };
+  // const checkValid = data => {!!!!!!!
+  //   // CHANGE 'VALID' PROPERTIES OF inputs ON BASE PREVIOUS VALUE
+  //   if (data.validity.patternMismatch === false) {!!!!!!!!!!
+  //     data.name === 'name'!!!!!!!!!!!
+  //       ? setInputNameValid(value => value && data.validity.patternMismatch)!!!!!!!
+  //       : setInputNumberValid(value => value && data.validity.patternMismatch);!!!!!!!!!!!!
+  //   } else {!!!!
+  //     data.name === 'name'!!!!!!!!!
+  //       ? setInputNameValid(value => value || data.validity.patternMismatch)!!!!!!1
+  //       : setInputNumberValid(value => value || data.validity.patternMismatch);!!!!!!!!!!!
+  //   }!!!!
+  // };!!!!!!!!
 
   const stateChange = data => {
     const { name, value } = data;
@@ -73,7 +73,7 @@ export const DataIn = () => {
 
   const inputChange = evt => {
     // change valid properties of inputs states, so that output users only, if bouth input fields contain valid value
-    checkValid(evt.target);
+    // checkValid(evt.target);!!!!!!!!!!!!!!!!!!!!!!!!!
     // change 'name' and 'number' fields in 'data'
     stateChange(evt.target);
   };
@@ -89,7 +89,7 @@ export const DataIn = () => {
             name="name"
             type="text"
             onChange={inputChange}
-            pattern="\w{0}[a-zA-Zа-яА-Я]+\s\w{0}[a-zA-Zа-яА-Я]+"
+            // pattern="\w{0}[a-zA-Zа-яА-Я]+\s\w{0}[a-zA-Zа-яА-Я]+"
             title="Please, use only letters and space in the following form: * *!"
             placeholder="Only letters # #"
             required
@@ -104,7 +104,7 @@ export const DataIn = () => {
             name="number"
             type="tel"
             onChange={inputChange}
-            pattern="\d{3}[0-9]-\d{1}[0-9]-\d{1}[0-9]"
+            // pattern="\d{3}[0-9]-\d{1}[0-9]-\d{1}[0-9]"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +
             in the following form: ####-##-##!"
             placeholder="Only digits ####-##-##"
